5107d0647135f6ee439b5770c96fe14b87fcde60
Lord Vollkorn <bertozzi@i-ways.de>
2020-05-12 19:57:18 +0200

    minor cli tuning

--- cli.sh
+++ cli.sh
@@ -7,18 +7,20 @@ if test $1 && test $1 == "sniff"; then
 	PEAR_EXCLUDE="--exclude=Generic.PHP.DisallowShortOpenTag,Generic.Files.LineLength";
 	#PEAR_EXCLUDE+=",PEAR.Commenting.ClassComment";
 	#PEAR_EXCLUDE+=",PEAR.Commenting.FunctionComment";
-	#PEAR_EXCLUDE+=",PEAR.NamingConventions.ValidFunctionName";
+	PEAR_EXCLUDE+=",PEAR.NamingConventions.ValidFunctionName";
 	#PEAR_EXCLUDE+=",PEAR.NamingConventions.ValidVariableName";
 
 	printf "\nTesting with PEAR standard:\n";
     $COMMAND $2 $EXTENSIONS --standard=PEAR $PEAR_EXCLUDE;
     printf "\nDONE!\n";
+
+    PSR2_EXCLUDE="--exclude=Generic.Files.LineLength,PSR1.Classes.ClassDeclaration,Squiz.Classes.ValidClassName"
     
     printf "\nTesting with PSR2 standard:\n";
-    $COMMAND $2 $EXTENSIONS --standard=PSR2;
+    $COMMAND $2 $EXTENSIONS --standard=PSR2 $PSR2_EXCLUDE;
     printf "\nDONE!\n";
 
-    #MEQP1_EXCLUDE="--exclude=Generic.Files.LineLength,Generic.PHP.DisallowShortOpenTag,Generic.PHP.Syntax";
+    MEQP1_EXCLUDE="--exclude=Generic.Files.LineLength";#,Generic.PHP.DisallowShortOpenTag,Generic.PHP.Syntax";
     
     printf "\nTesting with MEQP1 standard:\n";
     $COMMAND $2 $EXTENSIONS --standard=MEQP1 $MEQP1_EXCLUDE;
@@ -26,7 +28,7 @@ if test $1 && test $1 == "sniff"; then
 
     EXTENSIONS="--extensions=xml";
 
-    #PEAR_EXCLUDE="--exclude=Generic.Files.LineLength,Generic.PHP.DisallowShortOpenTag,PEAR.Commenting.FileComment"
+    PEAR_EXCLUDE="--exclude=Generic.Files.LineLength,Generic.PHP.DisallowShortOpenTag,PEAR.Commenting.FileComment"
 
     printf "\nTesting XML files with PEAR standard:\n";
     $COMMAND $2 $EXTENSIONS --standard=PEAR $PEAR_EXCLUDE;
@@ -38,7 +40,7 @@ if test $1 && test $1 == "sniff"; then
     $COMMAND $2 $EXTENSIONS --standard=PSR2 $PSR2_EXCLUDE;
     printf "\nDONE!\n";
 
-    #MEQP1_EXCLUDE="--exclude=Generic.Files.LineLength,Generic.PHP.DisallowShortOpenTag,Generic.PHP.Syntax";
+    MEQP1_EXCLUDE="--exclude=Generic.Files.LineLength,Generic.PHP.DisallowShortOpenTag,Generic.PHP.Syntax";
 
     printf "\nTesting XML files with MEQP1 standard:\n";
     $COMMAND $2 $EXTENSIONS --standard=MEQP1 $MEQP1_EXCLUDE;
