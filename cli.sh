3daa5860a50ae97f669652ec8b907502b5510514
Lord Vollkorn <bertozzi@i-ways.de>
2020-05-12 11:41:36 +0200

    mcs

---
+++ cli.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if test $1 && test $1 == "sniff"; then
+	COMMAND="mcs/vendor/bin/phpcs -s --colors";
+    EXTENSIONS="--extensions=css,js,json,php,phtml";
+
+	PEAR_EXCLUDE="--exclude=Generic.PHP.DisallowShortOpenTag,Generic.Files.LineLength";
+	#PEAR_EXCLUDE+=",PEAR.Commenting.ClassComment";
+	#PEAR_EXCLUDE+=",PEAR.Commenting.FunctionComment";
+	#PEAR_EXCLUDE+=",PEAR.NamingConventions.ValidFunctionName";
+	#PEAR_EXCLUDE+=",PEAR.NamingConventions.ValidVariableName";
+
+	printf "\nTesting with PEAR standard:\n";
+    $COMMAND $2 $EXTENSIONS --standard=PEAR $PEAR_EXCLUDE;
+    printf "\nDONE!\n";
+    
+    printf "\nTesting with PSR2 standard:\n";
+    $COMMAND $2 $EXTENSIONS --standard=PSR2;
+    printf "\nDONE!\n";
+
+    #MEQP1_EXCLUDE="--exclude=Generic.Files.LineLength,Generic.PHP.DisallowShortOpenTag,Generic.PHP.Syntax";
+    
+    printf "\nTesting with MEQP1 standard:\n";
+    $COMMAND $2 $EXTENSIONS --standard=MEQP1 $MEQP1_EXCLUDE;
+    printf "\nDONE!\n";
+
+    EXTENSIONS="--extensions=xml";
+
+    #PEAR_EXCLUDE="--exclude=Generic.Files.LineLength,Generic.PHP.DisallowShortOpenTag,PEAR.Commenting.FileComment"
+
+    printf "\nTesting XML files with PEAR standard:\n";
+    $COMMAND $2 $EXTENSIONS --standard=PEAR $PEAR_EXCLUDE;
+    printf "\nDONE!\n";
+
+    PSR2_EXCLUDE="--exclude=Generic.Files.LineLength,Generic.PHP.DisallowShortOpenTag";
+    
+    printf "\nTesting XML files with PSR2 standard:\n";
+    $COMMAND $2 $EXTENSIONS --standard=PSR2 $PSR2_EXCLUDE;
+    printf "\nDONE!\n";
+
+    #MEQP1_EXCLUDE="--exclude=Generic.Files.LineLength,Generic.PHP.DisallowShortOpenTag,Generic.PHP.Syntax";
+
+    printf "\nTesting XML files with MEQP1 standard:\n";
+    $COMMAND $2 $EXTENSIONS --standard=MEQP1 $MEQP1_EXCLUDE;
+    printf "\nDONE!\n";
+fi;
+
+printf "\n";
